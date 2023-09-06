3334c49ffec058a673e5a37cd46d0f2bfbb01f83
Kam Kennedy <kameronk013@gmail.com>
2023-09-06 09:29:05 -0600

    create student class passing all tests

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,21 @@
 class Student
-  
+  attr_reader :name, :age, :scores, :grade
+
+  def initialize(student_data)
+    @name = student_data[:name]
+    @age = student_data[:age]
+    @scores =[]
+    @grade = grade
+  end
+
+  def log_score(score)
+    @scores << score
+  end
+
+  def grade
+    if @scores != []
+      @grade = @scores.sum/@scores.size
+    end
+  end
+
 end
