7ba02ef0670676854137b73ecbfb8e6e91f128c5
Kam Kennedy <kameronk013@gmail.com>
2023-09-06 09:32:57 -0600

    add three tests to course spec

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -3,5 +3,19 @@ require './lib/course'
 require './lib/student'
 
 RSpec.describe Course do
-  
+  it "exists" do
+    course = Course.new("Calculus", 2)  
+    expect(course).to be_instance_of(Course)
+  end
+
+  it "has attributes" do
+    course = Course.new("Calculus", 2)
+    expect(course.name).to eq("Calculus")
+    expect(course.capacity).to eq 2
+  end
+
+  it "starts with no students" do
+    course = Course.new("Calculus", 2)
+    expect(course.students).to eq([])
+  end
 end
